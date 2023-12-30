c7a61e70c1ab844a17751be4009cd2be565d371e
M1ha Shvn <work_shvein_mihail@mail.ru>
2023-12-30 21:43:58 +0500

    Fix compatiblity with django 4.0+ (#33)
    1. Fix compatiblity with django 4.0+
    2. Removed django before 2.2 from testing
    3. Removed Postgres 9.4, 9.5,  10 from testing
    4. Removed Python 3.6 from testing

--- src/django_pg_returning/compatibility.py
+++ src/django_pg_returning/compatibility.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
+
 import django
-from typing import Type, Optional, List
+from typing import Type, Optional, List, Dict
 
 from django.db.models import Model, QuerySet, Field
 from django.db.models.sql import Query
@@ -36,3 +38,71 @@ def get_model_fields(model, concrete=False):  # type: (Type[Model], Optional[boo
             res = [f for f in res if getattr(f, 'concrete', True) and not getattr(f, 'many_to_many', False)]
 
     return res
+
+
+def clear_query_ordering(query):  # type: (Query) -> Query
+    """
+    Resets query ordering. Parameters changed in django 4.0
+    :param query: Query to change
+    :return: Resulting query
+    """
+    attr_name = 'force_empty' if django.VERSION < (4,) else 'force'
+    query.clear_ordering(**{attr_name: True})
+    return query
+
+
+def prepare_insert_query_kwargs(kwargs):
+    """
+    Prepares kwargs for InsertQuery method based on kwargs from QuerySet._insert(...)
+    :param kwargs: Original kwargs from QuerySet._insert(obj, fields, **kwargs)
+    :return: kwargs ready for InsertQuery(model, **kwargs)
+    """
+    if django.VERSION < (2, 2):
+        query_kwargs = {}
+    elif django.VERSION < (4, 1):
+        query_kwargs = {'ignore_conflicts': kwargs.get('ignore_conflicts')}
+    else:
+        query_kwargs = {
+            'on_conflict': kwargs.get('on_conflict'),
+            'update_fields': kwargs.get('update_fields'),
+            'unique_fields': kwargs.get('unique_fields')
+        }
+
+    return query_kwargs
+
+
+def get_not_deferred_fields(qs):  # type: (QuerySet) -> Dict[Type[Model], List[Field]]
+    """
+    Gets model fields for query
+    :param qs: QuerySet for which we get required fields
+    :return: A dictionary of lists {Model: [Field, Field, ...]}
+    """
+    fields = {}
+
+    if django.VERSION >= (4, 2):
+        fields = qs.query.get_select_mask()
+        result_fields = defaultdict(list)
+        for field in fields.keys():
+            result_fields[field.model].append(field)
+        fields = result_fields
+
+    elif django.VERSION >= (4, 1):
+        # Django 4.0 changed fields format
+        qs.query.deferred_to_data(fields)
+        fields = {
+            model: [
+                model._meta.get_field(field_name)
+                for field_name in field_names
+            ] for model, field_names in fields.items()
+        }
+
+    elif django.VERSION >= (1, 10):
+        qs.query.deferred_to_data(fields, qs._get_loaded_field_cb)
+
+    else:
+        # Before django 1.10 pk fields hasn't been returned from postgres.
+        # In this case, I can't match bulk_create results and return values by primary key.
+        # So I select all data from returned results
+        pass
+
+    return fields
